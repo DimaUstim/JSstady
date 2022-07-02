93ca79e0f31d4aa87427a4b3aa3da733cd9add87
Dima Ustimov <hccctt@gmail.com>
2022-07-02 14:26:58 +0300

    commit1

--- calculator/main.js
+++ calculator/main.js
@@ -1,10 +1,33 @@
-//calculator.js
+// First Task
+
+const $firstButton =  document.querySelector('#first-button')
+
+$firstButton.addEventListener("click", () => {
+    $firstButton.style.backgroundColor = "green";
+    $firstButton.style.color = "white";
+  });
+
+
+
+// Second Task
+
+const $secondButton =  document.querySelector('#second-button')
+
+  $secondButton.addEventListener("click", () => {
+      $secondButton.classList.toggle("green");
+     
+  });
+
+
+
+// Third Task
 
 const $number1 =  document.querySelector('#number1');
 const $number2 =  document.querySelector('#number2');
 const $operator =  document.querySelector('#operator');
 const $result =  document.querySelector('#result');
-const $button =  document.querySelector('#button');
+const $button =  document.querySelector('#button')
+
 $button.addEventListener('click', () => {
     const number1 = parseInt($number1.value);
     const number2 = parseInt($number2.value);
@@ -20,29 +43,8 @@ $button.addEventListener('click', () => {
     } else {
         $result.innerHTML = 'Error';
     }
-}
+  }
 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
