09659431a6255cf796b0f6b118b9f27558c9e73a
Dima Ustimov <hccctt@gmail.com>
2022-07-23 15:09:54 +0300

    last commit

--- module 3/3.1/js/main.js
+++ module 3/3.1/js/main.js
@@ -8,15 +8,26 @@
     }   //end of constructor function
 
     Person.prototype.sayName = function() {
-        console.log(this.name);
-        console.log(this.age);
+        return this.name;
     }  //end of prototype function
 
     const person = new Person("John", 30);
-    Object.defineProperty(person, "name",{configurable: false, writable: false, enumerable: false});
-    Object.defineProperty(person, "age",{configurable: false, writable: false, enumerable: false});
-    
-    person.sayName();
+    Object.defineProperties(person,{
+    name: {
+        configurable: false, 
+        writable: false, 
+        enumerable: false
+    },
+    age: {  
+        configurable: false, 
+        writable: false, 
+        enumerable: false
+    }
+    });
+
+    person.name="Jane";
+    delete person.name;
+
     console.log(person);
 
     //create a constructor function that creates an object with its own set of prototypes using Class
@@ -27,15 +38,26 @@
             this.name = name;
             this.age = age;
         }
-        sayName() {
-            console.log(this.name);
-            console.log(this.age);
+        sayAge() {
+            return this.age;
         } //end of prototype function
     }
 
     const person2 = new PersonClass("Kate", 40);
-    Object.defineProperty(person2, "name",{configurable: false, writable: false, enumerable: false});
-    Object.defineProperty(person2, "age",{configurable: false, writable: false, enumerable: false});
+    Object.defineProperties(person2,{
+        name: {
+            configurable: false, 
+            writable: false, 
+            enumerable: false
+        },
+        age: {  
+            configurable: false, 
+            writable: false, 
+            enumerable: false
+        }
+        });
     
-    person2.sayName();
+    person2.name="Natasha";
+    delete person2.age;
+
     console.log(person2);
