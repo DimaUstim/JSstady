0a0a0e52e7e29487a645f503c410af1aba047a2b
Dima Ustimov <hccctt@gmail.com>
2022-07-25 17:00:37 +0300

    commit change code

--- module 3/3.1/js/main.js
+++ module 3/3.1/js/main.js
@@ -1,63 +1,66 @@
-
-
 //create a constructor function that creates an object with its own set of prototypes
 
-    function Person(name, age) {
-        this.name = name;
-        this.age = age;
-    }   //end of constructor function
+function Person(options) {
+  this.name = options.name;
+  this.age = options.age;
+} //end of constructor function
 
-    Person.prototype.sayName = function() {
-        return this.name;
-    }  //end of prototype function
+Person.prototype.sayName = function () {
+  return this.name;
+}; //end of prototype function
 
-    const person = new Person("John", 30);
-    Object.defineProperties(person,{
-    name: {
-        configurable: false, 
-        writable: false, 
-        enumerable: false
-    },
-    age: {  
-        configurable: false, 
-        writable: false, 
-        enumerable: false
-    }
-    });
+const person = new Person({
+  name: "John",
+  age: 30,
+});
+Object.defineProperties(person, {
+  name: {
+    configurable: false,
+    writable: false,
+    enumerable: false,
+  },
+  age: {
+    configurable: false,
+    writable: false,
+    enumerable: false,
+  },
+});
 
-    person.name="Jane";
-    delete person.name;
+person.name = "Jane";
+delete person.name;
 
-    console.log(person);
+console.log(person);
 
-    //create a constructor function that creates an object with its own set of prototypes using Class
+//create a constructor function that creates an object with its own set of prototypes using Class
 
+class PersonClass {
+  constructor(options) {
+    this.name = options.name;
+    this.age = options.age;
+  }
+  sayAge() {
+    return this.age;
+  } //end of prototype function
+}
 
-    class PersonClass {
-        constructor(name, age) {
-            this.name = name;
-            this.age = age;
-        }
-        sayAge() {
-            return this.age;
-        } //end of prototype function
-    }
+const person2 = new PersonClass({
+  name: "Kate",
+  age: 40,
+});
+Object.defineProperties(person2, {
+  name: {
+    configurable: false,
+    writable: false,
+    enumerable: false,
+  },
+  age: {
+    configurable: false,
+    writable: false,
+    enumerable: false,
+  },
+});
 
-    const person2 = new PersonClass("Kate", 40);
-    Object.defineProperties(person2,{
-        name: {
-            configurable: false, 
-            writable: false, 
-            enumerable: false
-        },
-        age: {  
-            configurable: false, 
-            writable: false, 
-            enumerable: false
-        }
-        });
-    
-    person2.name="Natasha";
-    delete person2.age;
+person2.name = "Natasha";
+delete person2.age;
 
-    console.log(person2);
+console.log(person2);
